b3b55d98b66185b199214ed38c912efe12480335
Michał Górny <michal.x.gorny@gmail.com>
2022-01-24 19:07:24 +0100

    Initial Commit

---
+++ quizClassVer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  quizClassVer
+//
+//  Created by Michał GÓRNY on 24/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
